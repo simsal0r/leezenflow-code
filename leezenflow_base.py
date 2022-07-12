1908c1b9e3a2d0f84b938b5b5e714f005397b887
simeon.brueggenjuergen@gmail.com <Simeon>
2022-07-12 14:09:12 +0100

    WIP: MSHACK2022 animation

--- leezenflow_base.py
+++ leezenflow_base.py
@@ -16,10 +16,8 @@ from smoother import HoersterTorSmoother
 from simulations import Simulation
 
 class LeezenflowBase(object):
-    def __init__(self, *args, **kwargs):
-
-        try:
-        
+    def __init__(self, command_line_args):
+        try:    
             config = configparser.ConfigParser()
             config.read('config.ini')
 
@@ -30,37 +28,15 @@ class LeezenflowBase(object):
             self.mqtt_use_auth = config['mqtt']['use_auth']
             self.mqtt_client_user_name = config['mqtt']['client_user_name']
             self.mqtt_client_pw = config['mqtt']['client_pw']
-
         except KeyError:
             print("Configuration file for MQTT missing...")
+
+        self.args = command_line_args
             
-        # Use Dict such that data can be written at the same  time
+        # Use Dict such that data can be written at the same time
         self.shared_data = {
-            "current_phase" : "awaiting_message",
-            "remaining_time" : 30
+            "current_phase" : "awaiting_message"
         }
-
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("-r", "--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. Default: 32", default=32, type=int)
-        self.parser.add_argument("--led-cols", action="store", help="Panel columns. Typically 32 or 64. (Default: 32)", default=96, type=int)
-        self.parser.add_argument("-c", "--led-chain", action="store", help="Daisy-chained boards. Default: 1.", default=1, type=int)
-        self.parser.add_argument("-P", "--led-parallel", action="store", help="For Plus-models or RPi2: parallel chains. 1..3. Default: 1", default=1, type=int)
-        self.parser.add_argument("-p", "--led-pwm-bits", action="store", help="Bits used for PWM. Something between 1..11. Default: 11", default=11, type=int)
-        self.parser.add_argument("-b", "--led-brightness", action="store", help="Sets brightness level. Default: 100. Range: 1..100", default=100, type=int)
-        self.parser.add_argument("-m", "--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm" , choices=['regular', 'adafruit-hat', 'adafruit-hat-pwm'], default='adafruit-hat-pwm',type=str)
-        self.parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
-        self.parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
-        self.parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
-        self.parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 1", default=3, type=int) #3 best while testing at Swarco; 2 for local mqtt test
-        self.parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
-        self.parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
-        self.parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="Rotate:0", type=str)
-        self.parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1=AB-addressed panels; 2=row direct; 3=ABC-addressed panels; 4 = ABC Shift + DE direct", default=0, type=int, choices=[0,1,2,3,4])
-        self.parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan; 8=ZStripeUneven... (Default: 0)", default=0, type=int)
-        self.parser.add_argument("--led-panel-type", action="store", help="Needed to initialize special panels. Supported: 'FM6126A'", default="", type=str)
-
-        self.parser.add_argument("--test", action="store", help="Use test dataset. Default: -1; no test", default=-1, type=int)
-        self.parser.add_argument("--logging", action="store", help="1=Log all messages. Default:0 ", default=0, type=int)
 
     def mqtt_client(self,_,run_event):
         self.shared_data = {
@@ -106,8 +82,6 @@ class LeezenflowBase(object):
         print("Running")
 
     def process(self):
-
-        self.args = self.parser.parse_args()
 
         log_name = 'log/all.log'
         if self.args.logging == 1:
